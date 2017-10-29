727ffe7f7f108a86db9e6715844ea5f15b3f10fe
Almásy Gergő <almgergo@gmail.com>
2017-10-29 02:23:55 +0200

    phase 2

--- capnp/myClient.py
+++ capnp/myClient.py
@@ -4,7 +4,7 @@ import capnp
 import Bugfix_capnp
 import Request_capnp
 import Response_capnp
-import numpy as np
+from time import sleep
 
 HOST, PORT = "ecovpn.dyndns.org", 11223
 
@@ -17,32 +17,56 @@ def getLoginMessage():
     login.hash = 'r06176kpltk360ooeamvb6eauq1uu1taw9qh'
     return request
 
-def fixBugMessage(bugId):
+def fixBugMessage(bugCount):
     request = Request_capnp.Request.new_message()
     bugfix = request.init('bugfix')
-    bugfix.bugs = 1
+    bugfix.bugs = bugCount
     bugfix.message = 'Fixed'
     return request
 
+def establishConnection(cnt):
 # Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     # Connect to server and send data
-    print(getLoginMessage().to_bytes())
-    sock.connect((HOST, PORT))
-    sock.sendall(getLoginMessage().to_bytes())
+        sock.connect((HOST, PORT))
+        sock.sendall(getLoginMessage().to_bytes())
 
     # Receive data from the server and shut down
-    response = Response_capnp.Response.from_bytes(sock.recv(2048))
-    print (response)
+        response = Response_capnp.Response.from_bytes(sock.recv(2048))
+        # print (response)
+        # print (response.bugfix.bugs)
 
-    bugCount = response.bugfix.bugs
-    for i in range(0,1):
-        print('i: ' + str(i))
-        sock.sendall(fixBugMessage(10).to_bytes())
-        
+        i = response.bugfix.bugs-1
+        isEnd=False
+        while (not isEnd and i > -1):
+            # print('bugfix cnt: ' +str(i) + '\n')            
+            sock.sendall(fixBugMessage(i).to_bytes())
+            response = Response_capnp.Response.from_bytes(sock.recv(2048))
+            # print (response)
 
-    print(bugCount)
+            which = response.which()
+            # print('WHICH: ' + str(which) + (' bugs: ' + str(response.bugfix.bugs) if which=='bugfix' else  '' ))
+            if (which=='end'):
+                if (response.end=='true'):
+                    i = -1
+                    isEnd=True
+                    print('DONE')
+                else:
+                    i -= 1
+            elif (which=='bugfix'):
+                if (response.bugfix.bugs > 0 ):
+                    i = response.bugfix.bugs-1
+                else:
+                    i -= 1
 
-    response = Response_capnp.Response.from_bytes(sock.recv(2048))
-    print (response)
+        print('bugfix cnt: ' +str(cnt) + '\n')           
+        sock.sendall(fixBugMessage(cnt).to_bytes())
+        response = Response_capnp.Response.from_bytes(sock.recv(2048))
+        print (response)
+
+# for i in range(255,256):
+    # print('fixing ' + str(i) + ' bugs')
+establishConnection(255)
+    # sleep(0.3)
+    
     
